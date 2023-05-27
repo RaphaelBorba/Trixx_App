20fc20572d0e372a2e3077c657022056fe30a041
Raphael Borba <raphaelbsx@gmail.com>
2023-05-27 18:06:38 -0300

    feat: intial configs and react navigation add

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from './pages/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown:false}}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
